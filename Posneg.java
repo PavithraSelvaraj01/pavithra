a863ba1311881ec7a6c3bb5b8227aef5c74b5701
PavithraSelvaraj01 <pavithra4529@gmail.com>
2017-06-09 14:54:43 +0530

    Create Posneg.java

---
+++ Posneg.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Posneg
+{
+public static void main(String[] args)
+{
+Scanner scan=new scanner(system.in);
+int n=scan.nextInt();
+if(n>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("negative");
+}
+}
+}
