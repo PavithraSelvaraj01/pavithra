2fc121c07111831583fba647bd8964bf03e31179
PavithraSelvaraj01 <pavithra4529@gmail.com>
2017-06-11 10:55:51 +0530

    Create Rev.java

---
+++ Rev.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Rev
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        String s2[]=s1.split(" ");
+              System.out.print(s2[1]+"  "+s2[0]);
+    }
+}
