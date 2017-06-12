cf5cea79a3f26707da88c9d825b5f89403f5651e
PavithraSelvaraj01 <pavithra4529@gmail.com>
2017-06-12 15:33:57 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Palindrome
+{
+    public static void main(String[] args)
+    {
+        
+        Scanner scan=new Scanner(System.in);
+         int n=scan.nextInt();
+         int k=n;
+        int rem=0;
+        while(n>0)
+        {
+            rem=rem*10+n%10;
+            n=n/10;
+        }
+        if(k==rem)
+        {
+        System.out.println("palindrome");
+        }
+        else
+        {
+            System.out.println("not a palindrome");
+        }
+    }
+}
+
