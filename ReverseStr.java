cdb20d5ca318b552da73d2dd9fe011c9243c8a31
PavithraSelvaraj01 <pavithra4529@gmail.com>
2017-09-03 19:49:00 +0530

    Create ReverseStr.java

---
+++ ReverseStr.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class ReverseStr
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        StringBuffer s=new StringBuffer();
+        s.append(sc.nextLine());
+        StringBuffer s1=s.reverse();
+        String s2=String.valueOf(s1);
+        String res="";
+        for(int i=0;i<s2.length();i++)
+        {
+            char c=s2.charAt(i);
+            System.out.print(c);
+            if(i<s2.length()-1)
+            {
+                System.out.print("-");
+            }
+        }
+    }
+}
