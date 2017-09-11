ccdb69d3b8767f7a6599a4d5bd66d9f11fbbdf3a
PavithraSelvaraj01 <pavithra4529@gmail.com>
2017-09-11 11:40:09 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Armstrong
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+while(n>9)
+{
+while(n>0)
+{
+int r=n%10;
+
+}
+}
